e150552582534ea078b8d7286dec7fc4ea1e5618
Felipe Vilarinho <fvilarin@akamai.com>
2024-12-28 11:26:24 -0300

    Bug fixes and code improvements.

--- converter/src/main/java/com/akamai/ds2/converter/App.java
+++ converter/src/main/java/com/akamai/ds2/converter/App.java
@@ -61,7 +61,7 @@ public class App implements Runnable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SettingsUtil.getKafkaBrokers());
         properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, getId());
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         return properties;
     }
